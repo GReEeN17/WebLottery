7e28716c3ac3e4d951abdd14749532c3b55fa65f
Aleksandr <azelen04@mail.ru>
2024-02-14 19:09:10 +0300

    Added sequences for each entity

--- WebLottery.Infrastructure.Implementations/DataContext/DataContext.cs
+++ WebLottery.Infrastructure.Implementations/DataContext/DataContext.cs
@@ -20,13 +20,133 @@ public class DataContext : DbContext
     public DbSet<PocketTicketEntity> PocketTickets { get; set; }
     public DbSet<PrizeEntity> Prizes { get; set; }
     public DbSet<TicketEntity> Tickets { get; set; }
-    public DbSet<UserEntity> Uesrs { get; set; }
+    public DbSet<UserEntity> Users { get; set; }
     public DbSet<UserDrawEntity> UserDraws { get; set; }
     public DbSet<WalletEntity> Wallets { get; set; }
     public DbSet<WalletCurrencyEntity> WalletCurrencies { get; set; }
 
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasSequence<int>("currency_sequence")
+            .StartsAt(1)
+            .IncrementsBy(1)
+            .HasMin(1)
+            .HasMax(99999999999999999);
+        
+        modelBuilder.HasSequence<int>("draw_sequence")
+            .StartsAt(1)
+            .IncrementsBy(1)
+            .HasMin(1)
+            .HasMax(99999999999999999);
+        
+        modelBuilder.HasSequence<int>("pocket_sequence")
+            .StartsAt(1)
+            .IncrementsBy(1)
+            .HasMin(1)
+            .HasMax(99999999999999999);
+        
+        modelBuilder.HasSequence<int>("pocket_ticket_sequence")
+            .StartsAt(1)
+            .IncrementsBy(1)
+            .HasMin(1)
+            .HasMax(99999999999999999);
+        
+        modelBuilder.HasSequence<int>("prize_sequence")
+            .StartsAt(1)
+            .IncrementsBy(1)
+            .HasMin(1)
+            .HasMax(99999999999999999);
+        
+        modelBuilder.HasSequence<int>("ticket_sequence")
+            .StartsAt(1)
+            .IncrementsBy(1)
+            .HasMin(1)
+            .HasMax(99999999999999999);
+        
+        modelBuilder.HasSequence<int>("user_sequence")
+            .StartsAt(1)
+            .IncrementsBy(1)
+            .HasMin(1)
+            .HasMax(99999999999999999);
+        
+        modelBuilder.HasSequence<int>("wallet_sequence")
+            .StartsAt(1)
+            .IncrementsBy(1)
+            .HasMin(1)
+            .HasMax(99999999999999999);
+        
+        modelBuilder.HasSequence<int>("wallet_currency_sequence")
+            .StartsAt(1)
+            .IncrementsBy(1)
+            .HasMin(1)
+            .HasMax(99999999999999999);
+
+        #region CurrencyEntity config
+
+        modelBuilder.Entity<CurrencyEntity>().Property(pm => pm.Id)
+            .HasDefaultValueSql("nextval('\"currency_sequence\"')");
+
+        #endregion
+
+        #region DrawEntity config
+
+        modelBuilder.Entity<DrawEntity>().Property(pm => pm.Id)
+            .HasDefaultValueSql("nextval('\"draw_sequence\"')");
+
+        #endregion
+
+        #region PocketEntity config
+
+        modelBuilder.Entity<PocketEntity>().Property(pm => pm.Id)
+            .HasDefaultValueSql("nextval('\"pocket_sequence\"')");
+
+        #endregion
+
+        #region PocketTicketEntity config
+
+        modelBuilder.Entity<PocketTicketEntity>().Property(pm => pm.Id)
+            .HasDefaultValueSql("nextval('\"pocket_ticket_sequence\"')");
+
+        #endregion
+
+        #region PrizeEntity config
+
+        modelBuilder.Entity<PrizeEntity>().Property(pm => pm.Id)
+            .HasDefaultValueSql("nextval('\"prize_sequence\"')");
+
+        #endregion
+
+        #region TicketEntity config
+
+        modelBuilder.Entity<TicketEntity>().Property(pm => pm.Id)
+            .HasDefaultValueSql("nextval('\"ticket_sequence\"')");
+
+        #endregion
+
+        #region UserEntity config
+
+        modelBuilder.Entity<UserEntity>().Property(pm => pm.Id)
+            .HasDefaultValueSql("nextval('\"user_sequence\"')");
+
+        #endregion
+
+        #region WalletEntity config
+
+        modelBuilder.Entity<WalletEntity>().Property(pm => pm.Id)
+            .HasDefaultValueSql("nextval('\"wallet_sequence\"')");
+
+        #endregion
+
+        #region WalletCurrencyEntity config
+
+        modelBuilder.Entity<WalletCurrencyEntity>().Property(pm => pm.Id)
+            .HasDefaultValueSql("nextval('\"wallet_currency_sequence\"')");
+
+        #endregion
     }
 
     public async Task<int> SaveChangesAsync()
